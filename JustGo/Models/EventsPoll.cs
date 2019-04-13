1fa7de89e6e0132f261fbcdf855dbd995969b19b
Paul <pasha.fazluktinov@yandex.ru>
2019-04-13 23:22:31 +0700

    EventPoll added

---
+++ JustGo/Models/EventsPoll.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JustGo.View.Models
+{
+    public class EventsPoll
+    {
+        public long Id { get; set; }
+        public long Count { get; set; }
+        public List<Event> Results { get; set; }
+    }
+}
