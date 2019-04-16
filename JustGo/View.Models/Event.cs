efa2f34191e44680f2d12d292f7cc9d850125b98
Paul <pasha.fazluktinov@yandex.ru>
2019-04-16 23:03:24 +0700

    fix моделей

--- JustGo/View.Models/Event.cs
+++ JustGo/View.Models/Event.cs
@@ -10,7 +10,6 @@ namespace JustGo.View.Models
         public string Title { get; set; }
         public string Description { get; set; }
         public string Body_Text { get; set; } // полное описание
-        public string Address { get; set; }
         public Place Place { get; set; } // место проведения
         public List<string> Categories { get; set; } // список категорий
         public List<string> Tags { get; set; }
