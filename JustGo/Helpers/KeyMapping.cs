4ea39b3dcec36388bca4df70e63a382900a27efc
Andrey Orlovsky <andrey.orlovsky@gmail.com>
2019-04-17 23:43:03 +0700

    kudago <---> ours key mappings

---
+++ JustGo/Helpers/KeyMapping.cs
@@ -0,0 +1,20 @@
+﻿namespace JustGo.Helpers
+{
+    /// <summary>
+    /// Отображает ключи событий kudago на ключи в нашей базе
+    /// </summary>
+    public class EventsKeyMapping
+    {
+        public int KudagoId { get; set; }
+        public int OurId { get; set; }
+    }
+
+    /// <summary>
+    /// Отображает ключи мест kudago на ключи в нашей базе
+    /// </summary>
+    public class PlacesKeyMapping
+    {
+        public int KudagoId { get; set; }
+        public int OurId { get; set; }
+    }
+}
