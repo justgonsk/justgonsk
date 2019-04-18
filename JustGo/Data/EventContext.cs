23e8406b5cd30e487456d7634add6071ec983d9e
Andrey Orlovsky <andrey.orlovsky@gmail.com>
2019-04-18 13:46:26 +0700

    context & controller

---
+++ JustGo/Data/EventContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using JustGo.Helpers;
+using Microsoft.EntityFrameworkCore;
+using JustGo.Models;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace JustGo.Contexts
+{
+    public class EventContext : DbContext
+    {
+        public DbSet<Event> Events { set; get; }
+        public DbSet<Place> Places { set; get; }
+        public DbSet<Category> Categories { set; get; }
+        public DbSet<Tag> Tags { set; get; }
+
+        public EventContext(DbContextOptions<EventContext> options)
+            : base(options)
+        {
+            Debugger.Launch();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Event>().Property(t => t.Images).HasConversion(DbConverters.ImagesConverter);
+
+            modelBuilder.Entity<Coordinates>(builder =>
+            {
+                var baseProperties = typeof(Coordinates).BaseType.GetProperties().Select(p => p.Name);
+                var properties = typeof(Coordinates)
+                    .GetProperties(BindingFlags.Public | BindingFlags.DeclaredOnly | BindingFlags.Instance)
+                    .Select(p => p.Name);
+
+                foreach (var baseProperty in baseProperties.Except(properties))
+                {
+                    builder.Ignore(baseProperty);
+                }
+            });
+
+            modelBuilder.Entity<Place>().OwnsOne(place => place.Coordinates);
+
+            modelBuilder.Entity<EventDate>().ToTable("EventDates").HasKey(date => new { date.EventId, date.Start });
+        }
+    }
+}
---
+++ JustGo/Helpers/DbConverters.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JustGo.Models;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace JustGo.Helpers
+{
+    public static class DbConverters
+    {
+        /// <summary>
+        /// Транслирует список URL картинок в строку, разделённую '|' для хранения в нашей базе
+        /// При извлечении из базы воссоздаёт список моделей со ссылками
+        /// </summary>
+        ///
+        /// <remarks>
+        /// Отрефакторить этот говнокод не представляется возможным, потому что ValueConverter
+        /// работает не с просто лямбдами, а с деревьями выражений
+        /// </remarks>
+        public static ValueConverter<ICollection<ImageModel>, string> ImagesConverter { get; }
+            = new ValueConverter<ICollection<ImageModel>, string>
+        (
+            list => string.Join('|', list.Select(image => image.Image)),
+            wholeString => wholeString.Split('|', StringSplitOptions.None).Select(url => new ImageModel
+            {
+                Image = url
+            }).ToList()
+        );
+    }
+}
