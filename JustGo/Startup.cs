feadb65ce30405b0813e412a3d1e1f4a922267b3
Andrey Orlovsky <andrey.orlovsky@gmail.com>
2019-04-18 16:22:40 +0700

    IHostingEnvironment injected in Startup.cs for convenience

--- JustGo/Startup.cs
+++ JustGo/Startup.cs
@@ -17,12 +17,14 @@ namespace JustGo
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IHostingEnvironment environment)
         {
             Configuration = configuration;
+            Environment = environment;
         }
 
         public IConfiguration Configuration { get; }
+        public IHostingEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -35,14 +37,18 @@ namespace JustGo
 
             services.AddDbContext<MainContext>(options =>
             {
-                options.UseSqlServer(Configuration.GetConnectionString("LocalEventContext"));
+                var databaseConnStr = Environment.IsDevelopment() ? "LocalEventContext" : "TODO!!!";
+
+                var connectionString = Configuration.GetConnectionString(databaseConnStr);
+
+                options.UseSqlServer(connectionString);
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
+            if (Environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
