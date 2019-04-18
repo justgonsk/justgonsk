3e495318a3d91534881557c77ac3439bad907980
Andrey Orlovsky <andrey.orlovsky@gmail.com>
2019-04-19 02:41:13 +0700

    db repositories for events and places

---
+++ JustGo/Repositories/DbEventsRepository.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JustGo.Data;
+using JustGo.Helpers;
+using JustGo.Interfaces;
+using JustGo.Models;
+using JustGo.View.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JustGo.Repositories
+{
+    public class DbEventsRepository : IEventsRepository
+    {
+        private readonly MainContext context;
+
+        public DbEventsRepository(MainContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Event> EnumerateAll() => context.Events;
+
+        public Poll<EventViewModel> GetEventPoll(EventsFilter filter = null,
+            int? offset = 0, int? count = 100)
+        {
+            var wholeSequence = filter?.FilterEvents(this) ?? EnumerateAll();
+
+            var limitedSequence = wholeSequence
+                .Skip(offset ?? 0).Take(count ?? 100)
+                .AsViewModels<Event, EventViewModel>();
+
+            return limitedSequence.ToPoll();
+        }
+
+        public async Task<Event> AddAsync(EventViewModel viewModel)
+        {
+            var newEvent = viewModel.ToModel();
+
+            await AssignProperties(newEvent, viewModel);
+
+            context.Events.Add(newEvent);
+
+            await context.SaveChangesAsync();
+
+            return newEvent;
+        }
+
+        public async Task<Event> FindAsync(int id)
+            => await context.Events.FindAsync(id);
+
+        public async Task<Event> UpdateAsync(int id, EventViewModel viewModel)
+        {
+            var eventToUpdate = context.Events.Find(id);
+
+            await AssignProperties(eventToUpdate, viewModel);
+
+            await context.SaveChangesAsync();
+
+            return eventToUpdate;
+        }
+
+        public async Task<Event> DeleteAsync(int id)
+        {
+            var eventToRemove = context.Events.Find(id);
+
+            context.Events.Remove(eventToRemove);
+
+            await context.SaveChangesAsync();
+
+            return eventToRemove;
+        }
+
+        private async Task AssignProperties(Event @event, EventViewModel viewModel)
+        {
+            //если место с таким ID не найдено, будет брошено исключение
+            var place = await context.Places.FirstAsync(existingPlace => existingPlace.Id == viewModel.Place.Id);
+
+            @event.Title = viewModel.Title;
+            @event.ShortTitle = viewModel.ShortTitle;
+            @event.Description = viewModel.Description;
+            @event.Dates = new List<EventDate>(viewModel.Dates);
+            @event.Images = new List<ImageModel>(viewModel.Images);
+
+            @event.Place = place;
+
+            @event.EventCategories = viewModel.Categories.Select(categoryName => new EventCategory
+            {
+                //если такой категории ещё не было, она будет добавлена при сохранении; аналогично с тегом
+                Category = FindCategoryByName(categoryName) ?? new Category { Name = categoryName },
+                Event = @event
+            }).ToHashSet();
+
+            @event.EventTags = viewModel.Tags.Select(tagName => new EventTag
+            {
+                Tag = FindTagByName(tagName) ?? new Tag { Name = tagName },
+                Event = @event
+            }).ToHashSet();
+        }
+
+        private Category FindCategoryByName(string desiredName)
+        {
+            return context.Categories.FirstOrDefault(cat => cat.Name == desiredName);
+        }
+
+        private Tag FindTagByName(string desiredName)
+        {
+            return context.Tags.FirstOrDefault(tag => tag.Name == desiredName);
+        }
+    }
+}
---
+++ JustGo/Repositories/DbPlacesRepository.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JustGo.Data;
+using JustGo.Helpers;
+using JustGo.Interfaces;
+using JustGo.Models;
+using JustGo.View.Models;
+
+namespace JustGo.Repositories
+{
+    internal class DbPlacesRepository : IPlacesRepository
+    {
+        private readonly MainContext context;
+
+        public DbPlacesRepository(MainContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Place> EnumerateAll() => context.Places;
+
+        public Poll<PlaceViewModel> GetPlacePoll(int? offset = 0, int? count = 100)
+        {
+            var wholeSequence = EnumerateAll();
+
+            var limitedSequence = wholeSequence
+                .Skip(offset ?? 0).Take(count ?? 100)
+                .AsViewModels<Place, PlaceViewModel>();
+
+            return limitedSequence.ToPoll();
+        }
+
+        public async Task<Place> AddAsync(PlaceViewModel viewModel)
+        {
+            var newPlace = new Place();
+
+            AssignProperties(newPlace, viewModel);
+
+            context.Places.Add(newPlace);
+
+            await context.SaveChangesAsync();
+
+            return newPlace;
+        }
+
+        public async Task<Place> FindAsync(int id)
+            => await context.Places.FindAsync(id);
+
+        public async Task<Place> UpdateAsync(int id, PlaceViewModel viewModel)
+        {
+            var placeToUpdate = context.Places.Find(id);
+
+            AssignProperties(placeToUpdate, viewModel);
+
+            await context.SaveChangesAsync();
+
+            return placeToUpdate;
+        }
+
+        public async Task<Place> DeleteAsync(int id)
+        {
+            var placeToRemove = context.Places.Find(id);
+
+            context.Places.Remove(placeToRemove);
+
+            await context.SaveChangesAsync();
+
+            return placeToRemove;
+        }
+
+        private void AssignProperties(Place place, PlaceViewModel viewModel)
+        {
+            place.Title = viewModel.Title;
+            place.Address = viewModel.Address;
+            place.Coordinates = new Coordinates
+            {
+                Latitude = viewModel.Coordinates.Latitude,
+                Longitude = viewModel.Coordinates.Longitude
+            };
+        }
+    }
+}
