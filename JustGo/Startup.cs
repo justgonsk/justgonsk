bcb0f9e93e885fd190c5f2eb615b474756f7249a
Andrey Orlovsky <andrey.orlovsky@gmail.com>
2019-04-19 18:14:52 +0700

    json parses/returns in snake_case

--- JustGo/Startup.cs
+++ JustGo/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using JustGo.Data;
+using JustGo.Helpers;
 using JustGo.Interfaces;
 using JustGo.Repositories;
 using Microsoft.AspNetCore.Builder;
@@ -31,7 +32,13 @@ namespace JustGo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddJsonOptions(options =>
+                {
+                    options.SerializerSettings.ContractResolver = Utilities.SnakeCaseSettings.ContractResolver;
+                });
+
             services.AddHttpContextAccessor();
 
             services.AddCors();
