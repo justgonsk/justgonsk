53ac24752d635eceaaf04526e00b82930463ac16
Andrey Orlovsky <andrey.orlovsky@gmail.com>
2019-04-20 04:16:39 +0700

    new migration: "start" in eventdates now can be null

--- JustGo/Migrations/20190419191542_Second.Designer.cs
+++ JustGo/Migrations/20190419191542_Second.Designer.cs
@@ -4,14 +4,16 @@ using JustGo.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace JustGo.Migrations
 {
     [DbContext(typeof(MainContext))]
-    partial class MainContextModelSnapshot : ModelSnapshot
+    [Migration("20190419191542_Second")]
+    partial class Second
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ JustGo/Migrations/20190419191542_Second.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JustGo.Migrations
+{
+    public partial class Second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_EventDates",
+                table: "EventDates");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Start",
+                table: "EventDates",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AddColumn<int>(
+                name: "EventDateId",
+                table: "EventDates",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_EventDates",
+                table: "EventDates",
+                column: "EventDateId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventDates_EventId",
+                table: "EventDates",
+                column: "EventId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_EventDates",
+                table: "EventDates");
+
+            migrationBuilder.DropIndex(
+                name: "IX_EventDates_EventId",
+                table: "EventDates");
+
+            migrationBuilder.DropColumn(
+                name: "EventDateId",
+                table: "EventDates");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Start",
+                table: "EventDates",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_EventDates",
+                table: "EventDates",
+                columns: new[] { "EventId", "Start" });
+        }
+    }
+}
