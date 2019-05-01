3bd5dcb8f0b22c0aed2336bd137289fee8e76c4c
Andrey Orlovsky <andrey.orlovsky@gmail.com>
2019-05-01 16:14:36 +0700

    new classes

---
+++ JustGoModels/Models/View/Schedule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JustGoModels.Models.View
+{
+    public class Schedule
+    {
+        public List<int> DaysOfWeek { get; set; }
+        public TimeSpan? StartTime { get; set; }
+        public TimeSpan? EndTime { get; set; }
+    }
+}
